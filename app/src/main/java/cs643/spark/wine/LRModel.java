e717a446a95b97f098687652297e79bda0b5e723
Shawn Cicoria <github@cicoria.com>
2022-11-18 17:14:57 -0500

    ore

---
+++ app/src/main/java/cs643/spark/wine/LRModel.java
@@ -0,0 +1,11 @@
+package cs643.spark.wine;
+
+import org.apache.spark.ml.classification.LogisticRegression;
+import org.apache.spark.ml.classification.LogisticRegressionModel;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class LRModel {
+    
+}//https://spark.apache.org/docs/latest/ml-classification-regression.html#logistic-regression
